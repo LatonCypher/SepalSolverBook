1caaf33f1954e67bb5a8ad8c7277d646ff2ce38c
Lateef Kareem <lateef.a.kareem@cyphercrescent.com>
2026-01-09 04:39:37 +0100

    book set up completed

--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using ConsoleApp1;
-using ConsoleApp1.TrainingFiles;
-using ConsoleApp1.TrainingFiles.Chapter_0_Basic_Operations_Syntax;
-using ConsoleApp1.TrainingFiles.Chapter_1_Interpolation;
-using ConsoleApp1.TrainingFiles.Chapter_1_Polynomials;
-using ConsoleApp1.TrainingFiles.Chapter_4_Linear_Algerba;
-using ConsoleApp1.TrainingFiles.Summary_and_Conclusion.Advanced_Problems_In_Process_Engineering;
 
 
 Writer.Run();
