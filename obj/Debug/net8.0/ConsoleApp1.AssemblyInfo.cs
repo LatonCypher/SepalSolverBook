21efd02415f3bab702fb0c248c0e39bab59cf0ed
Lateef Kareem <lateef.a.kareem@cyphercrescent.com>
2026-01-20 06:45:10 +0100

    updated

--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+969d65be5807959b10a215c76d2a7d769032dadb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a80ed482d825dd0ae3822c8f643c0070f80dc91")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
