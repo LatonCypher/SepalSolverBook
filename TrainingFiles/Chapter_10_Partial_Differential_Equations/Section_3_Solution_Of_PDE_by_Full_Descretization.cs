945e5d99dcb0eed8b5a385f708b2c8d0bcff29de
Lateef Kareem <lateef.a.kareem@cyphercrescent.com>
2026-01-20 11:09:53 +0100

    intros done

---
+++ TrainingFiles/Chapter_10_Partial_Differential_Equations/Section_3_Solution_Of_PDE_by_Full_Descretization.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.TrainingFiles.Chapter_10_Partial_Differential_Equations
+{
+    internal class Section_3_Solution_Of_PDE_by_Full_Descretization
+    {
+    }
+}
--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+241446062bb246d6f927f17b464fb192086a7971")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ef39f925dcd45dff85127c7e251bbb7f63ca31c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
