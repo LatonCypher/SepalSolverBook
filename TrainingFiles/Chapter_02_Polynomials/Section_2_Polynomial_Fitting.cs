7fa565308f83cf13667b5bc2091a2aef262ea7ba
sanniyunusa <sanni.yunusa@cyphercrescent.net>
2026-01-20 09:21:37 +0100

    Add example problem on quadratic interpolation

--- TrainingFiles/Chapter_02_Polynomials/Section_2_Polynomial_Fitting.cs
+++ TrainingFiles/Chapter_02_Polynomials/Section_2_Polynomial_Fitting.cs
@@ -10,8 +10,15 @@
             {
                 // Example of polynomial fitting
                 double[] x = [1, 2, 3, 4, 5], y = [3, 4, 5, 6, 7];
-                double[] fit = Polyfit(x, y, 1);
+                double[] fit = Polyfit(x, y, 1); //This is a linear fit since degree of polynomial is 1
                 Console.WriteLine($"Polynomial fit: {fit}");
+            }
+            {
+                // Example of quadratic polynomial fitting
+                double[] x = [0, 0.5, 1.0, 1.5, 2, 2.5, 3], y = [2, 14.375, 22, 24.875, 23, 16.375, 5];
+                double[] fit = Polyfit(x, y, 2); //This is a quadratic fit since degree of polynomial is 2
+                Console.WriteLine($"Quadratic Polynomial fit: {fit}");
+
             }
         }
     }
