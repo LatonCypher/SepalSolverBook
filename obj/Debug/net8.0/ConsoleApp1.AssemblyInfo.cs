89c9a2e24742b5a8ef82b9b74f5cd310080b59f5
Lateef Kareem <lateef.a.kareem@cyphercrescent.com>
2026-02-02 05:26:44 +0100

    integer printing

--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbabbd6d76f25fc181015867326d802aa2b790b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71534f78a46cb0d172020556c300a2dc478a90ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
