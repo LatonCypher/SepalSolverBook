bcd890bdb331aa360d70bd1210808c16e175bc42
Lateef Kareem <lateef.a.kareem@cyphercrescent.com>
2026-02-04 18:31:09 +0100

    correct matrix printing

--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1778bc49d6571b0c73f4aeb2e1b51580c006b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac7f9585b0ccc8963bb0e49c002a942f340dfe23")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
