8fc637ac07a76cf3608d71f612534579cafdf3ed
Lateef Kareem <lateef.a.kareem@cyphercrescent.com>
2026-02-12 03:56:52 +0100

    daes

--- TrainingFiles/Chapter_08_Ordinary_Differential_Equations/Section_6_Differential_Algebraic_Equations.cs
+++ TrainingFiles/Chapter_08_Ordinary_Differential_Equations/Section_6_Differential_Algebraic_Equations.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ScottPlot;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static SepalSolver.Ode;
 
 namespace ConsoleApp1.TrainingFiles.Chapter_08_Ordinary_Differential_Equations
 {
@@ -11,6 +13,216 @@ namespace ConsoleApp1.TrainingFiles.Chapter_08_Ordinary_Differential_Equations
         public static void Run()
         {
             /// <BookContent>
+            ///
+            /// <header 1> 1. Introduction to DAEs </header>
+            /// Differential-Algebraic Equations are a class of functional equations that contain both differential equations (describing the evolution of the system) and algebraic constraints (restricting the state space). Unlike standard Ordinary Differential Equations (ODEs), DAEs are not explicitly solved for all derivatives.
+            ///
+            /// A general DAE system is expressed in the implicit form: :math:`F(t, y, y') = 0`
+            ///
+            /// If the Jacobian :math:`\frac{\partial F}{\partial y'}` is non-singular, the system is essentially an implicit ODE. If it is singular, the system is a "true" DAE.
+            ///
+            /// <header 2> 2. The Concept of Index </header>
+            /// The difficulty of solving a DAE is measured by its **index**. The most common definition is the **differentiation index**: the number of times you must differentiate the algebraic constraints to express the system as a set of explicit ODEs.
+            /// * **Index 0:** An ODE.
+            /// * **Index 1:** The most common solvable DAE (e.g., the algebraic variables can be solved for directly).
+            /// * **Higher Index (2+):** These are numerically unstable and usually require index reduction techniques before solving.
+            ///
+            ///
+            ///
+            /// <header 2> 3. Solving DAEs with `sepalsolver` </header>
+            /// In modern computational environments like C#, DAEs can be solved using the `SepalSolver` library, which utilizes a Mass Matrix formulation:
+            /// :math:`M y' = f(t, y)`
+            /// Where :math:`M` is a singular matrix.
+            ///
+            /// ---
+            ///
+            /// <header 2> 4. Examples and Applications </header>
+            ///
+            /// <example 1>
+            /// **Example 1: The Robertson Problem (Chemical Kinetics)**
+            /// This is a classic stiff DAE representing the reaction of three species. It is an Index-1 DAE where the total mass is conserved via an algebraic constraint.
+            /// 
+            ///
+            /// <code>
+            {
+                double[] robertson_f(double t, double[] y) =>
+                    [(-0.04 * y[0] + 1e4 * y[1] * y[2]),
+                     (0.04 * y[0] - 1e4 * y[1] * y[2] - 3e7 * y[1]*y[1]),
+                     y[0] + y[1] + y[2] - 1.0];
+
+                double[,] mass_f(double t, double[] y) => new double[,] 
+                {
+                    { 1.0, 0.0, 0.0 },
+                    { 0.0, 1.0, 0.0 },
+                    { 0.0, 0.0, 0.0 }
+                };
+
+                double[] y0 = [1.0, 0.0, 0.0];
+                (ColVec T, Matrix Y) = Ode45a(robertson_f, mass_f, y0, [0, 1e7]);
+                // Plot the result
+                Y[.., 1] = 1e4*Y[.., 1];
+                SemiLogx(T, Y);
+                Xlabel("Time t"); Ylabel("Soluton y");
+                Legend(["y_1", "1e4*y_2", "y_3"], MiddleLeft);
+                Title("Solution of Robertson's ODE with ODE45a");
+                SaveAs("Robertson-ODE-given-points-Ode45a.png");
+            }
+            /// </code>
+            /// </example>
+            ///
+            /// <example 1> The Simple Pendulum (Index-1)
+            /// A pendulum in Cartesian coordinates is naturally an Index-3 DAE. We solve the stabilized Index-1 version by including velocity constraints.
+            ///
+            ///
+            /// <code>
+            {
+                double g = 9.81;
+            
+                // State vector y = [x, y, u, v, lambda]
+                double[] pendulum_f(double t, double[] y) =>
+                    [y[2],
+                     y[3],
+                     -y[0] * y[4],
+                     -y[1] * y[4] - g,
+                     y[2]*y[2] + y[3]*y[3] - y[1] * g - y[4]];
+            
+                double[,] mass_f(double t, double[] y) => new double[,]
+                {
+                    { 1, 0, 0, 0, 0 },
+                    { 0, 1, 0, 0, 0 },
+                    { 0, 0, 1, 0, 0 },
+                    { 0, 0, 0, 1, 0 },
+                    { 0, 0, 0, 0, 0 }
+                };
+
+                double[] y0 = [0, 1, 1, 0, -1];
+                (ColVec T, Matrix Y) = Ode45a(pendulum_f, mass_f, y0, [0, 10.2]);
+                Plot(T, Y);
+                Xlabel("x"); Ylabel("y");
+                Title("Pendulum Trajectory (DAE)");
+                SaveAs("Index_1-Pendulum-Problem-Ode45a.png");
+            }
+            /// </code>
+            /// </example>
+            ///
+            /// <example 2> Semi-Explicit DAE (The Transistor Amplifier)**
+            /// This example mimics the "hbdae" problem from MathWorks, representing an electrical circuit with nonlinear components.
+            ///
+            /// <code>
+            {
+                double Ub = 6, R0 = 1000, R15 = 9000, alpha = 0.99,
+                    beta = 1e-6, Uf = 0.026, c1 = 1e-6, c2 = 2e-6, c3 = 3e-6;
+                double[,] Mass(double t, double[] y) => new double[,] 
+                {
+                    {-c1,  c1,  0,   0,   0 },
+                    { c1, -c1,  0,   0,   0 },
+                    { 0,   0,  -c2,  0,   0 },
+                    { 0,   0,   0,  -c3,  c3},
+                    { 0,   0,   0,   c3, -c3} 
+                };
+
+                double[] dudt(double t, double[] u)
+                {
+                    double Ue = 0.4 * Sin(200 * pi * t),
+                           f23 = beta * (Exp((u[1] - u[2]) / Uf) - 1);
+                    return [ -(Ue - u[0])/R0,
+                             -(Ub/R15 - u[1]*2/R15 - (1-alpha)*f23),
+                             -(f23 - u[2]/R15),
+                             -((Ub - u[3])/R15 - alpha*f23),
+                             u[4]/R15 ];
+                }
+                double[] tspan = [0, 0.1];
+                double[] y0 = [0, Ub / 2, Ub / 2, Ub, 0];
+
+                var opts = Odeset(RelTol: 1e-3, MassType: Ode.MassType.Constant);
+
+                (ColVec T, Matrix Y) = Ode45a(dudt, Mass, y0, tspan, opts);
+                ColVec X = T, U5 = Y["", 4];
+                Scatter(X, 0.4 * Sin(200 * pi * X), "o"); HoldOn();
+                Plot(X, U5, "--r"); HoldOff();
+                Legend(["Input", "Output"], UpperLeft);
+                Xlabel("Time t"); Ylabel("Solution y");
+                Title("One Transistor Amplifier DAE Problem-Ode45a");
+                SaveAs("One-Transistor-Amplifier-DAE-Problem-Ode45a.png");
+            }
+            /// </code>
+            /// </example>
+            ///
+            /// <example 3> The Akzo Nobel Problem
+            /// A high-dimensional DAE describing a chemical process with 6 differential and 2 algebraic equations. This tests the solver's ability to handle stiff systems with coupled variables.
+            ///
+            /// **Mathematical Description:**
+            /// The system is defined by reaction rates :math:`r_i` and concentrations :math:`y_1, ..., y_8`:
+            /// 
+            /// <math>
+            ///     \begin{array}{rcl}
+            ///         r_1 &=& k_1 \cdot y_1^4 \cdot y_2^{0.5}\\
+            ///         r_2 &=& k_2 \cdot y_3 \cdot y_4 \\
+            ///         r_3 &=& k_2 / K \cdot y_1 \cdot y_5\\
+            ///         r_4 &=& k_3 \cdot y_1 \cdot y_4^2\\
+            ///         r_5 &=& k_4 \cdot y_6^2 \cdot y_2^{0.5}
+            ///     \end{array}
+            /// </math>
+            ///
+            /// The differential equations are:
+            /// <math>
+            ///     \begin{array}{rcl}
+            ///         y_1' &=& -2r_1 + r_2 - r_3 - r_4\\
+            ///         y_2' &=& -0.5r_1 - r_5 + 0.5F_{in}\\
+            ///         y_3' &=& r_1 - r_2 + r_3\\
+            ///         y_4' &=& -r_2 + r_3 - 2r_4\\
+            ///         y_5' &=& r_2 - r_3 + r_4\\
+            ///         y_6' &=& -r_5
+            ///     \end{array}
+            /// </math>
+            ///
+            /// The algebraic constraints (Equilibrium):
+            /// <math>
+            ///     \begin{array}{rcl}
+            ///         0 &=& y_1 \cdot y_3 - y_7\\
+            ///         0 &=& y_4 \cdot y_5 - y_8
+            ///     \end{array}
+            /// </math>
+            /// 
+            /// <code>
+            {
+                double k1 = 18.7, k2 = 0.58, k3 = 0.09, k4 = 0.42, K = 34.4, Fin = 0.012;
+                double r1(double[] y) => k1 * Pow(y[0], 4) * Pow(y[1], 0.5);
+                double r2(double[] y) => k2 * y[2] * y[3];
+                double r3(double[] y) => (k2 / K) * y[0] * y[4];
+                double r4(double[] y) => k3 * y[0] * Pow(y[3], 2);
+                double r5(double[] y) => k4 * Pow(y[5], 2) * Pow(y[1], 0.5);
+                double[] akzo_f(double t, double[] y) =>
+                    [
+                        -2*r1(y) + r2(y) - r3(y) - r4(y),
+                        -0.5*r1(y) - r5(y) + 0.5*Fin,
+                        r1(y) - r2(y) + r3(y),
+                        -r2(y) + r3(y) - 2*r4(y),
+                        r2(y) - r3(y) + r4(y),
+                        -r5(y),
+                        y[0] * y[2] - y[6],
+                        y[3] * y[4] - y[7]
+                    ];
+                
+            
+                double[,] mass_f(double t, double[] y)
+                {
+                    var M = Zeros(8, 8);
+                    for(int i = 0; i < 6; i++) M[i, i] = 1.0;
+                    return M;
+                }
+            
+                double[] y0 = [0.444, 0.0012, 0.0, 0.0037, 0.0, 0.0, 0.0, 0.0];
+                (ColVec T, Matrix Y) = Ode45a(akzo_f, mass_f, y0, [0, 180]);
+            
+                Plot(T, Y);
+                Xlabel("Time"); Ylabel("Concentration");
+                Title("Akzo Nobel Chemical Kinetics (DAE)");
+                SaveAs("Akzo-Nobel-Ode45a.png");
+            }
+            /// </code>
+            /// </example>
+            /// 
             /// 
             /// </BookContent>
         }
--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0672935de8c3352fde7ee1a3680969d16774a2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb296c9536862977da84059909b8966266a90916")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
