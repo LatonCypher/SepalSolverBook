1a983d8b5eaacbb632381dd5965c25b8af01187a
Lateef Kareem <lateef.a.kareem@cyphercrescent.com>
2026-02-19 09:04:59 +0100

    twice

--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d23c481131956f5e1329871e5d53307889b2cae4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d4b022faa97a16eb62e578b8366091a613b2ff5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
