32d55075f82725a47e211f23e9538bcde3cca452
Lateef Kareem <lateef.a.kareem@cyphercrescent.com>
2026-05-16 20:15:16 +0100

    vector

--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e828b9ac13e48b52be9c06cbeade92d374c0411")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e93d71ff73061e23033a7589caa99258c24f021")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
