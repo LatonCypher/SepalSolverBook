2929b879c733500779f98be179377dac4f55c699
Lateef Kareem <96069639+LatonCypher@users.noreply.github.com>
2026-08-16 12:55:17 +0100

    auto diff for lambertw function

--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a299a3c313e13a8051ba7ef1a15436aeab114f86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c649095356d31fff5724eee607ceaeb357c09768")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
