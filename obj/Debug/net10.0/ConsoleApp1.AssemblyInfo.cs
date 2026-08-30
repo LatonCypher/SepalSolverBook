de69d95e0eed75fb30bf9f5ef0ba3165a4ac4aa4
Lateef Kareem <96069639+LatonCypher@users.noreply.github.com>
2026-08-30 11:09:29 +0100

    yaml added

--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4992b06872d977dbec0ce4ade9256ea18c3e0166")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e29db899924a1230894f3831985d1aa795baf9f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
