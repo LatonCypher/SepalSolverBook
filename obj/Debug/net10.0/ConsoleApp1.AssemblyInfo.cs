3a106849544e74092b9a7f1a3c10aef33a145d0f
Lateef Kareem <96069639+LatonCypher@users.noreply.github.com>
2026-09-04 08:39:17 +0100

    chore: generate assembly info and build cache files for project compilation

--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a1180255eda3c4f5f41d8aa35e3f2588accda0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cf44d5fddd578dd956ab85ceb103fdcd557f31c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
